27e9df440a2e7798e71887933d53015757a12243
Akhil Byju <akhil.byju1@gmail.com>
2025-12-03 17:10:24 -0800

    organize setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,15 @@
 import { View, Text, Button } from "react-native";
 import { useState } from "react";
 import axios from "axios";
+import Constants from 'expo-constants';
+
+const apiURL = Constants.expoConfig?.extra?.apiURL || "";
 
 export default function Index() {
   const [message, setMessage] = useState("");
 
   const apiCall = () => {
-    axios.get('http://localhost:8080').then((data) => {
+    axios.get(apiURL).then((data) => {
       console.log(data)
       setMessage(data.data);
     })
