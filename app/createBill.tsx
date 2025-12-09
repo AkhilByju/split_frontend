4da98d06861d3ded725034dd38cd437b41cb30f3
Akhil Byju <akhil.byju1@gmail.com>
2025-12-08 19:24:37 -0800

    add safety navigation

--- app/createBill.tsx
+++ app/createBill.tsx
@@ -2,10 +2,24 @@ import { View, Text, StyleSheet } from "react-native";
 
 const CreateBill = () => {
   return (
-    <View>
-        <Text>createBill</Text>
+    <View style={styles.container}>
+        <Text style={styles.title}>createBill</Text>
     </View>
   )
 }
 
 export default CreateBill;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",     
+        justifyContent: "flex-start", 
+        paddingTop: 100,      
+        backgroundColor: "#fff",  
+    },
+    title: {
+        marginBottom: 40,
+        fontSize: 32, 
+    },
+});
--- app/joinBill.tsx
+++ app/joinBill.tsx
@@ -2,10 +2,24 @@ import { View, Text, StyleSheet } from "react-native";
 
 const JoinBill = () => {
   return (
-    <View>
-        <Text>joinBill</Text>
+    <View style={styles.container}>
+        <Text style={styles.title}>joinBill</Text>
     </View>
   )
 }
 
-export default JoinBill
+export default JoinBill;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",     
+        justifyContent: "flex-start", 
+        paddingTop: 100,      
+        backgroundColor: "#fff",  
+    },
+    title: {
+        marginBottom: 40,
+        fontSize: 32, 
+    },
+});
